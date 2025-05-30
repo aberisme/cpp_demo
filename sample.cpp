d2aa1f8a0e74abadf3737b496c8e4e6f28b6b757
aberisme <abraham.magpantay@gmail.com>
2025-05-31 00:31:10 +0800

    update cout my-branch

--- sample.cpp
+++ sample.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World, I am C++!" << endl;
+    cout << "Hello World, I am my-branch!" << endl;
     cout << "Running on thread ... " << this_thread::get_id() << endl;
 
+    system("pause > 0");
     return 0;
 }
